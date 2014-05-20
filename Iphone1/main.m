86c7023c7a04fe319f9547246726e45fe531c2e6
ashish ranjan <Ranjan@ashishs-air.recc.uc.edu>
2014-05-20 10:51:42 -0400

    Initial Commit

---
+++ Iphone1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Iphone1
+//
+//  Created by ashish ranjan on 5/20/14.
+//  Copyright (c) 2014 University of Cincinnati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCAppDelegate class]));
+    }
+}
---
+++ Iphone1Tests/Iphone1Tests.m
@@ -0,0 +1,34 @@
+//
+//  Iphone1Tests.m
+//  Iphone1Tests
+//
+//  Created by ashish ranjan on 5/20/14.
+//  Copyright (c) 2014 University of Cincinnati. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Iphone1Tests : XCTestCase
+
+@end
+
+@implementation Iphone1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
